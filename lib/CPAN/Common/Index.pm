2191c6351a42e99dae3b03552562cff0c2132365
David Golden <dagolden@cpan.org>
2013-04-14 18:05:39 +0100

    initial import

---
+++ lib/CPAN/Common/Index.pm
@@ -0,0 +1,35 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package CPAN::Common::Index;
+# ABSTRACT: No abstract given for CPAN::Common::Index
+# VERSION
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+=for Pod::Coverage method_names_here
+
+=head1 SYNOPSIS
+
+  use CPAN::Common::Index;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+Maybe other modules do related things.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et:
