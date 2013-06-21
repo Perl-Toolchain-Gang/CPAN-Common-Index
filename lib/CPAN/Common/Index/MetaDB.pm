371aff69a124f626ec08042d1b41fc999f3df16d
David Golden <dagolden@cpan.org>
2013-06-21 14:09:12 -0400

    add CPAN MetaDB index backend

---
+++ lib/CPAN/Common/Index/MetaDB.pm
@@ -0,0 +1,85 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package CPAN::Common::Index::MetaDB;
+# ABSTRACT: Search index via CPAN MetaDB
+# VERSION
+
+use parent 'CPAN::Common::Index';
+
+use Carp;
+use CPAN::Meta::YAML;
+use HTTP::Tiny;
+
+sub attributes {
+    return {
+        uri => "http://cpanmetadb.plackperl.org/v1.0/"
+    };
+}
+
+sub validate_attributes {
+    my ($self) = @_;
+
+    # ensure URI ends in '/'
+    my $uri = $self->uri;
+    $uri =~ s{/?$}{/};
+    $self->uri($uri);
+
+    return 1;
+}
+
+sub search_packages {
+    my ( $self, $args ) = @_;
+    Carp::croak("Argument to search_packages must be hash reference")
+      unless ref $args eq 'HASH';
+
+    # only support direct package query
+    return unless keys %$args == 1 && exists $args->{package} && ref $args->{package} eq '';
+
+    my $mod = $args->{package};
+    my $res = HTTP::Tiny->new->get($self->uri . "package/$mod");
+    return unless $res->{success};
+
+    if ( my $yaml = CPAN::Meta::YAML->read_string($res->{content}) ) {
+        my $meta = $yaml->[0];
+        if ($meta && $meta->{distfile}) {
+            my $file = $meta->{distfile};
+            $file =~ s{^./../}{}; # strip leading
+            return {
+                package => $mod,
+                version => $meta->{version},
+                uri     => "cpan:///distfile/$file",
+            };
+        }
+    }
+
+    return;
+}
+
+sub search_authors { return }; # not supported
+
+__PACKAGE__->_build_accessors;
+
+=for Pod::Coverage method_names_here
+
+=head1 SYNOPSIS
+
+  use CPAN::Common::Index::MetaDB;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+Maybe other modules do related things.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et:
---
+++ t/metadb.t
@@ -0,0 +1,59 @@
+use 5.008001;
+use strict;
+use warnings;
+use Test::More 0.96;
+use Test::FailWarnings;
+use Test::Deep '!blessed';
+use Test::Fatal;
+
+use Cwd qw/getcwd/;
+use File::Temp;
+use File::Spec::Functions qw/catfile/;
+use HTTP::Tiny;
+
+my $test_url = "http://cpanmetadb.plackperl.org/v1.0/package/File::Marker";
+
+plan skip_all => "Can't reach CPAN MetaDB"
+  unless HTTP::Tiny->new->get($test_url)->{success};
+
+require_ok("CPAN::Common::Index::MetaDB");
+
+subtest "constructor tests" => sub {
+    # no arguments, all defaults
+    new_ok(
+        'CPAN::Common::Index::MetaDB' => [],
+        "new with no args"
+    );
+
+    # uri specified
+    new_ok(
+        'CPAN::Common::Index::MetaDB' => [ { uri => "http://example.com" } ],
+        "new with cache"
+    );
+
+    # unknown argument
+    eval { CPAN::Common::Index::MetaDB->new( { foo => 'bar' } ) };
+    like(
+        $@ => qr/Unknown arguments to new\(\): foo/,
+        "Unknown argument dies with error"
+    );
+
+};
+
+subtest 'find package' => sub {
+    my $index = new_ok("CPAN::Common::Index::MetaDB");
+
+    my $got = $index->search_packages( { package => 'Moose' } );
+    ok( $got,                "found package" );
+    ok( $got->{version} > 2, "has a version" );
+    like(
+        $got->{uri},
+        qr{^cpan:///distfile/\w+/Moose-\d+\.\d+\.tar.gz$},
+        "uri format looks OK"
+    );
+
+};
+
+done_testing;
+# COPYRIGHT
+# vim: ts=4 sts=4 sw=4 et:
